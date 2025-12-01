f1b68667a7529786869b4cbc53f72defa9808a96
cubarm840-spec <cubarm840@gmail.com>
2025-12-01 16:02:08 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from telegram.ext import Application, CommandHandler
+from telegram import InputFile
+import os
+
+# Получаем токен из переменных окружения Render
+TOKEN = os.environ.get("BOT_TOKEN")
+
+async def start(update, context):
+    chat_id = update.effective_chat.id
+    with open("photo.jpg", "rb") as img:
+        await context.bot.send_photo(
+            chat_id=chat_id,
+            photo=InputFile(img),
+            caption="Привет! Это стартовое сообщение с картинкой 🔥"
+        )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Бот запущен на Render!")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
